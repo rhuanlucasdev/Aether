523ba23d50b3e8f7fad9fbde6c82eccc557e7488
Rhuan Lucas <rhuanlucassbs@gmail.com>
2026-07-01 17:43:01 -0300

    feat(catalog): add product listing, filters and product pages

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,19 @@
 import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
-const Homepage = () => {
+const Homepage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ category: string }>;
+}) => {
+  const category = (await searchParams).category;
+
   return (
     <div className="">
       <div className="relative aspect-[2/1] mb-12">
         <Image src="/featuredPrd.png" alt="Featured Product" fill />
       </div>
-      <ProductList />
+      <ProductList category={category} params="homepage" />
     </div>
   );
 };
--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -2,6 +2,8 @@ import { ProductsType } from "@/types";
 import React from "react";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
+import Link from "next/link";
+import Filter from "./Filter";
 
 // TEMPORARY
 const products: ProductsType = [
@@ -115,15 +117,28 @@ const products: ProductsType = [
   },
 ];
 
-const ProductList = () => {
+const ProductList = ({
+  category,
+  params,
+}: {
+  category: string;
+  params: "homepage" | "products";
+}) => {
   return (
     <div className="">
       <Categories />
+      {params === "products" && <Filter />}
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
-        {products.map((product => (
-            <ProductCard key={product.id} product={product} />
-        )))}
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
+      <Link
+        href={category ? `/products/?category=${category}` : `/products`}
+        className="flex justify-end mt-4 underline text-sm text-gray-500"
+      >
+        View All Products
+      </Link>
     </div>
   );
 };
