65c4204b1e0bcf7e69b42a8fea4283b1a430369e
Rhuan Lucas <rhuanlucassbs@gmail.com>
2026-07-05 00:32:26 -0300

    feat(cart): implement cart store with add, remove, and clear functionalities feat(ui): integrate react-toastify for success notifications on adding products fix(ui): update ShoppingCartIcon to display the correct item count

--- client/src/components/ShoppingCartIcon.tsx
+++ client/src/components/ShoppingCartIcon.tsx
@@ -1,14 +1,16 @@
 "use client";
+import useCartStore from "@/stores/cartStore";
 import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const ShoppingCartIcon = () => {
+  const { cart } = useCartStore();
   return (
     <Link href="/cart" className="relative">
       <ShoppingCart className="w-4 h-4 text-gray-600" />
       <span className="absolute -top-3 -right-3 bg-blue-400 text-white w-4 h-4 rounded-full flex items-center justify-center text-xs font-medium">
-        0
+        {cart.length}
       </span>
     </Link>
   );
--- client/src/types.ts
+++ client/src/types.ts
@@ -51,3 +51,13 @@ export const paymentFormSchema = z.object({
 });
 
 export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
+
+export type CartStoreStateType = {
+  cart: CartItemsType;
+};
+
+export type CartStoreActionsType = {
+  addToCart: (product: CartItemType) => void;
+  removeFromCart: (product: CartItemType) => void;
+  clearCart: () => void;
+};
